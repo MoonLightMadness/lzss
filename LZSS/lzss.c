343520afbfb3c80d9f772ad19b9cee10b30d3ce9
ZhangHL-q <qq2377459837@163.com>
2021-05-25 22:49:34 +0800

    算法初步

--- LZSS/lzss.c
+++ LZSS/lzss.c
@@ -1,4 +1,20 @@
+#include<stdio.h>
 
-void read_file(char* text,char filepath,int len) {
-
+char* read_file(char* filepath,int offset,int len) {
+	FILE *p;
+	fopen_s(p, filepath, "r+");
+	fseek(p, offset, SEEK_CUR);
+	char* data = (char*)malloc(sizeof(char)*len);
+	for (int i = 0; i < len; i++) {
+		int res = fgetc(p);
+		if (res != -1) {
+			data[i] = res;
+		}
+		else
+		{
+			break;
+		}
+	}
+	fclose(p);
+	return data;
 }
